2a5afc16597726397a700e56421c3469f28f6f48
Furuya <Furuka0928@gmail.com>
2020-11-16 10:14:11 +0900

    変数名修正等

--- fujimura/fujimuraQ.java
+++ fujimura/fujimuraQ.java
@@ -3,11 +3,11 @@ public class fujimuraQ{
 	public static void main(String[] args){
 		System.out.print("何人>");
 		int num=new Scanner(System.in).nextInt();
-		final int Balls_Num=99;
-		int[] balls=new int[Balls_Num];
-		int[] hit=new int [num];
-		String[] p=new String[num];
-		for(int i=0;i<Balls_Num;i++){
+		final int balls_Num=99;
+		int[] balls=new int[balls_Num];
+		int[] score=new int [num];
+		String[] player=new String[num];
+		for(int i=0;i<balls_Num;i++){
 			balls[i]=i+1;
 		}
 		for(int i=0;i<balls.length;i++){
@@ -19,24 +19,23 @@ public class fujimuraQ{
 		for(int i=0;i<num;i++){
 			System.out.printf("%d人目の名前>",i+1);
 			String name=new Scanner(System.in).nextLine();
-			p[i]=name;
-			hit[i]=balls[i];
-			System.out.printf("%sさんの点数:%d点",name,balls[i]);
+			player[i]=name;
+			score[i]=balls[i];
+			System.out.printf("%sさんの点数:%d点%n",name,balls[i]);
 			System.out.println();
-			System.out.println("");
 		}
-		for(int i=0;i<hit.length;i++){
-			for(int j=i+1;j<hit.length;j++){
-				if(hit[i] < hit[j]){
-					int temp=hit[i];
-					String temp2=p[i];
-					hit[i]=hit[j];
-					p[i]=p[j];
-					hit[j]=temp;
-					p[j]=temp2;
+		for(int i=0;i<score.length;i++){
+			for(int j=i+1;j<score.length;j++){
+				if(score[i] < score[j]){
+					int temp=score[i];
+					String temp2=player[i];
+					score[i]=score[j];
+					player[i]=player[j];
+					score[j]=temp;
+					player[j]=temp2;
 				}
 			}
-					System.out.printf("%d位:%d点:%sさん%n",i+1,hit[i],p[i]);
+					System.out.printf("%d位:%d点:%sさん%n",i+1,score[i],player[i]);
 					//pullreq出来なかったので文字追加
 		}
 	}
