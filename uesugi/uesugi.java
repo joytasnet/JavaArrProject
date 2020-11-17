0c3e060667ac39606d013a3c9ad74ce3a593c0d4
kyoko <edgwcnn4869@gmail.com>
2020-11-17 17:20:03 +0900

    ソースコード修正

--- uesugi/uesugi.java
+++ uesugi/uesugi.java
@@ -1,16 +1,23 @@
 import java.util.*;
- public class MakeQ{
+ public class uesugi{
 	 public static void main(String[] args){
-     String[] when = new String[]{"昨夜未明、","10年前、","今夜","年明けに"};
-     String[] who = new String[]{"私が","おじいさんが","50代男性が","みんなが"};
-     String[] where = new String[]{"代々木公園のベンチで、","六本木駅で","映画館で","異世界で"};
-     String[] what = new String[]{"一日中眠る","暴れる","踊る","怒られる"};
-     int[] temp = new int[when.length];
-		 for (int i = 0; i < when.length; i++){
-			int ran = new Random().nextInt(when.length);
-			temp[i] = ran;
-		}
-		System.out.println(when[temp[0]]+who[temp[1]]+where[temp[2]]+what[temp[3]]);
+		 String[] items = {"いつ","誰が","どこで","何をした",};
+		 String[][] data = new String[items.length][];
+		 for(int i = 0; i < data.length; i++){
+			 System.out.printf("%sはいくつ>",items[i]);
+			 int count = new Scanner(System.in).nextInt();
+			 data[i] = new String[count];
+			 for(int j = 0; j < data[i].length; j++){
+				 System.out.printf("%sをいれて>",items[i]);
+				 data[i][j] = new Scanner(System.in).nextLine();
+			 }
+		 }
+		 String[] seps = {"、","が、","で、","。",};
+		 for(int i = 0; i < data.length; i++){
+				 int index = new Random().nextInt(data[i].length);
+				 System.out.print(data[i][index]+seps[i]);
+			 }
+			 System.out.println();
 	}
 }
 
